4911b3f093484265612caf9d8c67ff3093388b76
poovilikarthika <107428566+poovilikarthika@users.noreply.github.com>
2022-11-27 23:19:52 +0530

    updated index jsp files

--- src/main/java/com/Accio/Search.java
+++ src/main/java/com/Accio/Search.java
@@ -17,6 +17,35 @@ import java.util.ArrayList;
 @WebServlet("/Search")
 public class Search extends HttpServlet {
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String keyword = request.getParameter("keyword");
+        ArrayList<SearchResult> results = new ArrayList<SearchResult>();
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            ResultSet resultSet = connection.createStatement().executeQuery("select pagetitle, pagelink, (length(lower(pagedata))-length(replace(lower(pagedata), '" + keyword + "', '')))/length('" + keyword + "') as countoccurence from pages order by countoccurence desc limit 30;");
+            while (resultSet.next()) {
+                SearchResult searchResult = new SearchResult();
+                searchResult.setName(resultSet.getString("pagetitle"));
+                searchResult.setLink(resultSet.getString("pagelink"));
+                results.add(searchResult);
+            }
+            PreparedStatement preparedStatement = connection.prepareStatement("Insert into History values (?, ?)");
+            preparedStatement.setString(1, keyword);
+            preparedStatement.setString(2, "https://poovili-search-engine.herokuapp.com/Search?keyword=" + keyword);
+            preparedStatement.executeUpdate();
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException);
+        }
+
+
+        request.setAttribute("results", results);
+        request.getRequestDispatcher("/Search.jsp").forward(request, response);
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+    }
+}
+/*
     protected void doGet(HttpServletRequest request, HttpServletResponse response){
 
         String keyword = request.getParameter("keyword");
@@ -56,4 +85,4 @@ public class Search extends HttpServlet {
         }
 
     }
-}
+}*/
